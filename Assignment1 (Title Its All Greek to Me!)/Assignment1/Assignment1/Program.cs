f7d455fcc4be1067761b80ce0e0a6e2b8ba847dd
aaron1986 <aaronbrucesmith86@gmail.com>
2016-04-23 16:56:03 +0100

    First commit

---
+++ Assignment1 (Title Its All Greek to Me!)/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pythagorean theorem c^2 = a^2 + b^2
+            //thus c = square root(a^2 + b^2)
+            Console.Write("Enter");
+            float point1X2 = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //float tan = point1X2 * point1X2;
+
+            Console.Write("Enter");
+            float point2Y2 = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //float cos = point2Y2 * point2Y2;
+
+            float tan = (float)Math.Sqrt(point1X2 * point1X2 + point2Y2 * point2Y2);
+
+            Console.WriteLine("Answer + ", tan);
+        }
+    }
+}
